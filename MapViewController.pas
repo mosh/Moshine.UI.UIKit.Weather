47a038e713acb15bc5f36b72f83e9a201c83b420
John Moshakis <john.moshakis@firmex.com>
2019-12-02 23:50:02 -0500

    make timeline optional

--- MapViewController.pas
+++ MapViewController.pas
@@ -10,7 +10,37 @@ type
   [IBObject]
   MapViewController = public class(UIViewController, IAWFWeatherMapDelegate, IAWFTimelineViewDelegate)
   private
+
+    method setBoat(zoomLocation:CLLocationCoordinate2D;name:String);
+    begin
+      //N16°50.16', W57°37.7'
+      var METERS_PER_MILE := 1609.344;
+
+      // 2
+      var viewRegion := MKCoordinateRegionMakeWithDistance(zoomLocation, 500*METERS_PER_MILE, 500*METERS_PER_MILE);
+
+      // 3
+      if (assigned(self.weatherMapView.mapView))then
+      begin
+        var view := self.weatherMapView.mapView;
+        if(view is MKMapView)then
+        begin
+          var mapView := view as MKMapView;
+          mapView.setRegion(viewRegion) animated(true);
+
+          var boat := new MKPointAnnotation;
+          boat.title := name;
+          boat.coordinate := zoomLocation;
+          mapView.addAnnotation(boat);
+          mapView.zoomEnabled := true;
+        end;
+      end;
+
+    end;
+
   public
+
+    property showTimeline:Boolean := false;
 
     property weatherMapView:AWFWeatherMap;
     property timelineView:AWFTimelineView;
@@ -41,15 +71,27 @@ type
       weatherMapView.delegate := self;
       self.view.addSubview(weatherMapView.weatherMapView);
 
-      // AWFMapLayer.
       weatherMapView.addSourceForLayerType(AWFMapLayerWindSpeeds);
+      weatherMapView.addSourceForLayerType(AWFMapLayerWindDirection);
+      weatherMapView.addSourceForLayerType(AWFMapLayerWindGusts);
 
-      timelineView := new AWFTimelineView WithFrame(CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 50.0));
-      timelineView.delegate := self;
-      timelineView.startDate := weatherMapView.timeline.fromTime;
-      timelineView.endDate := weatherMapView.timeline.toTime;
-      timelineView.currentTime := weatherMapView.timeline.fromTime;
-      self.view.addSubview(timelineView);
+      if(showTimeline)then
+      begin
+
+        timelineView := new AWFTimelineView WithFrame(CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 50.0));
+        timelineView.delegate := self;
+        timelineView.startDate := weatherMapView.timeline.fromTime;
+        timelineView.endDate := weatherMapView.timeline.toTime;
+        timelineView.currentTime := weatherMapView.timeline.fromTime;
+        self.view.addSubview(timelineView);
+
+        var timelineHeight: CGFloat := 50.0;
+        timelineView.translatesAutoresizingMaskIntoConstraints := false;
+        timelineView.preservesSuperviewLayoutMargins := true;
+        NSLayoutConstraint.activateConstraints([timelineView.topAnchor.constraintEqualToAnchor(self.view.layoutMarginsGuide.bottomAnchor) constant(-timelineHeight), timelineView.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor), timelineView.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor), timelineView.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor)]);
+        self.timelineView.playButton.addTarget(self) action(selector(toggleAnimation:)) forControlEvents(UIControlEvents.TouchUpInside);
+        self.weatherMapView.goToTime(NSDate.date());
+      end;
 
       weatherMapView.weatherMapView.translatesAutoresizingMaskIntoConstraints := false;
       NSLayoutConstraint.activateConstraints([weatherMapView.weatherMapView.topAnchor.constraintEqualToAnchor(self.view.topAnchor),
@@ -57,40 +99,8 @@ type
                                               weatherMapView.weatherMapView.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor),
                                               weatherMapView.weatherMapView.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor)]);
 
-      var timelineHeight: CGFloat := 50.0;
-      timelineView.translatesAutoresizingMaskIntoConstraints := false;
-      timelineView.preservesSuperviewLayoutMargins := true;
-      NSLayoutConstraint.activateConstraints([timelineView.topAnchor.constraintEqualToAnchor(self.view.layoutMarginsGuide.bottomAnchor) constant(-timelineHeight), timelineView.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor), timelineView.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor), timelineView.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor)]);
-      self.timelineView.playButton.addTarget(self) action(selector(toggleAnimation:)) forControlEvents(UIControlEvents.TouchUpInside);
-      self.weatherMapView.goToTime(NSDate.date());
 
       setBoat(self.Location, self.Name);
-
-    end;
-
-    method setBoat(zoomLocation:CLLocationCoordinate2D;name:String);
-    begin
-      //N16°50.16', W57°37.7'
-      var METERS_PER_MILE := 1609.344;
-
-      // 2
-      var viewRegion := MKCoordinateRegionMakeWithDistance(zoomLocation, 500*METERS_PER_MILE, 500*METERS_PER_MILE);
-
-      // 3
-      if (assigned(self.weatherMapView.mapView))then
-      begin
-        var view := self.weatherMapView.mapView;
-        if(view is MKMapView)then
-        begin
-          var mapView := view as MKMapView;
-          mapView.setRegion(viewRegion) animated(true);
-
-          var boat := new MKPointAnnotation;
-          boat.title := name;
-          boat.coordinate := zoomLocation;
-          mapView.addAnnotation(boat);
-        end;
-      end;
 
     end;
 
@@ -109,7 +119,7 @@ type
       end;
     end;
 
-    method timelineView(timelineView: AWFTimelineView) didPanToDate(date: NSDate);
+    method timelineView(timelineViewParam: AWFTimelineView) didPanToDate(date: NSDate);
     begin
       if self.weatherMapView.config.timelineScrubbingEnabled then
       begin
@@ -118,7 +128,7 @@ type
       end;
     end;
 
-    method timelineView(timelineView: AWFTimelineView) didSelectDate(date: NSDate);
+    method timelineView(timelineViewParam: AWFTimelineView) didSelectDate(date: NSDate);
     begin
       self.weatherMapView.stopAnimating();
       self.weatherMapView.goToTime(date);
@@ -143,36 +153,54 @@ type
 
     method weatherMapDidResetAnimation(weatherMapParam: AWFWeatherMap);
     begin
-      self.timelineView.setProgress(0.0) animated(true);
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.setProgress(0.0) animated(true);
+      end;
     end;
 
     method weatherMap(weatherMapParam : AWFWeatherMap) animationDidUpdateToDate(date: NSDate);
     begin
-      self.timelineView.currentTime := date;
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.currentTime := date;
+      end;
     end;
 
     method weatherMapDidStartLoadingAnimationData(weatherMap: AWFWeatherMap);
     begin
-      self.timelineView.playButton.selected := true;
-      self.timelineView.showLoading(true);
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.playButton.selected := true;
+        self.timelineView.showLoading(true);
+      end;
     end;
 
     method weatherMapDidFinishLoadingAnimationData(weatherMap: AWFWeatherMap);
     begin
-      self.timelineView.setProgress(1.0) animated(true);
-      self.timelineView.showLoading(false);
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.setProgress(1.0) animated(true);
+        self.timelineView.showLoading(false);
+      end;
     end;
 
     method weatherMapDidCancelLoadingAnimationData(weatherMap: AWFWeatherMap);
     begin
-      self.timelineView.playButton.selected := false;
-      self.timelineView.setProgress(0.0) animated(true);
-      self.timelineView.showLoading(false);
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.playButton.selected := false;
+        self.timelineView.setProgress(0.0) animated(true);
+        self.timelineView.showLoading(false);
+      end;
     end;
 
     method weatherMap(weatherMap: AWFWeatherMap) didUpdateAnimationDataLoadingProgress(totalLoaded: NSInteger) total(total: NSInteger);
     begin
-      self.timelineView.setProgress(CGFloat(totalLoaded) / CGFloat(total)) animated(true);
+      if(assigned(self.timelineView))then
+      begin
+        self.timelineView.setProgress(CGFloat(totalLoaded) / CGFloat(total)) animated(true);
+      end;
     end;
 
 
